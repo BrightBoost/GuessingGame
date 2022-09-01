f7e48b803e94e2178fc34a9d8a51ce0a0c81fe0c
brightboost <info@brightboost.nl>
2022-09-01 14:50:01 +0200

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
-$(document).ready(function(){
-  $("#newRoundBtn").on("click", startNewRound);
+$(document).ready(function () {
+  $("#newRoundBtn").on("click", function () {
+    let correctGuess = Math.floor(Math.random() * 100) + 1;
+    startNewRound(correctGuess);
+  });
 });
 
-function startNewRound() {
+function startNewRound(correctGuess) {
   // make sure all the fields are cleared
   $("#messageDiv").html("");
   $("#playerGuess").html("");
